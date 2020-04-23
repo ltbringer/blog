9002a74b12c58afd0a4b5c308a6200f8f34ab1cc
AmreshVenugopal <amresh.venugopal@gmail.com>
2020-04-23 08:24:30 +0530

    update: allow captions in images

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,8 @@ module.exports = {
           {
             resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 690
+              maxWidth: 690,
+              showCaptions: true
             }
           },
           {
