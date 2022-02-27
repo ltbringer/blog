56f67c04bff02eb38e2c2a2c479f0c26acab154b
ltbringer <amresh.venugopal@gmail.com>
2022-02-27 16:36:01 +0530

    add: mermaid deps.

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
+          "gatsby-remark-mermaid",
           {
             resolve: "gatsby-remark-images",
             options: {
