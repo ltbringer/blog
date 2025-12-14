c51f2f65b51fcb05fac24695a58daaf9a6d039dc
ltbringer <amresh.venugopal@gmail.com>
2025-12-14 04:05:15 +0000

    update: config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			title: 'Ltbringer',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/ltbringer' }],
+			customCss: ['./src/styles/custom.css'],
 		}),
 	],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 });
